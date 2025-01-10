49d746bdabfa098b1502cc1cc1f3e5ad076ea3ef
Your Name <you@example.com>
2025-01-10 16:05:49 +0000

    add storage!!

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,5 +1,5 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
+import { storage } from './storage/resource';import { auth } from './auth/resource';
 import { data } from './data/resource';
 
 /**
@@ -8,4 +8,5 @@ import { data } from './data/resource';
 defineBackend({
   auth,
   data,
+  storage
 });
